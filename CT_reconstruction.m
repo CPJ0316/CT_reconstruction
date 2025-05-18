af34353773966ce3fc1bd2a2f56994f8250ef4ec
CPJ0316 <0910424128j@gmail.com>
2025-05-18 17:04:23 +0800

    update .m file and update corrected .py file

--- CT_reconstruction.m
+++ CT_reconstruction.m
@@ -1,5 +1,5 @@
 
-img=imread("D:\1成大\0課程\113-2\醫學影像系統\HW2\toy.jpg");
+img=imread("./image.jpg");
 img_resized=imresize(img,[512,512]);
 if size(img_resized,3)==3
     img_gray=rgb2gray(img_resized);
@@ -13,9 +13,20 @@ theta_03=0:0.3:179;
 %calculate sinogram
 [R_3,xp_3]=radon(img_gray,theta_3);
 [R_03,xp_03]=radon(img_gray,theta_03);
+
 %show sinogram
 figure;%new window
-subplot(2, 3, 1);
+subplot(2, 4, 1);
+% left image
+imshow(img_gray, []);
+title('Original Image');
+% right image
+subplot(2, 4, 5);
+imshow(img_gray, []);
+title('Original Image');
+
+
+subplot(2, 4, 2);
 % left image
 imagesc(theta_3,xp_3,R_3);
 xlabel('Angle (degrees)');
@@ -24,7 +35,7 @@ title('Sinogram3');
 colormap gray;
 colorbar;
 % right image
-subplot(2, 3, 4);
+subplot(2, 4, 6);
 imagesc(theta_03,xp_03,R_03);
 xlabel('Angle (degrees)');
 ylabel('Sensor Position');
@@ -32,17 +43,16 @@ title('Sinogram03');
 colormap gray;
 colorbar;
 
-% back-projection
+% back-projection, no filter
 recon_unfiltered_3 = iradon(R_3, theta_3, 'linear', 'none');  % 'none' = no filter
 recon_unfiltered_03 = iradon(R_03, theta_03, 'linear', 'none');
 
-%figure;%new window
 % left image
-subplot(2, 3, 2);
+subplot(2, 4, 3);
 imshow(recon_unfiltered_3, []);
 title('Sinogram3');
 % right image
-subplot(2, 3, 5);
+subplot(2, 4, 7);
 imshow(recon_unfiltered_03, []);
 title('Sinogram03');
 
@@ -51,10 +61,10 @@ recon_filtered_3 = iradon(R_3, theta_3, 'linear', 'Ram-Lak');  % 'none' = no fil
 recon_filtered_03 = iradon(R_03, theta_03, 'linear', 'Ram-Lak');
 
 % left image
-subplot(2, 3, 3);
+subplot(2, 4, 4);
 imshow(recon_filtered_3, []);
 title('Sinogram3 filtered');
 % right image
-subplot(2, 3, 6);
+subplot(2, 4, 8);
 imshow(recon_filtered_03, []);
 title('Sinogram03 filtered');
--- CT_reconstruction_self.py
+++ CT_reconstruction_self.py
@@ -6,70 +6,79 @@ from skimage.transform import resize
 from scipy.ndimage import rotate
 import matplotlib.pyplot as plt
 
-'''
-def radon_transform(image, theta):
-    num_detectors = image.shape[0]
-    sinogram = np.zeros((len(theta), num_detectors))
-    for i, angle in enumerate(theta):
-        rotated = rotate(image, -angle, reshape=False, order=1)
-        sinogram[i, :] = np.sum(rotated, axis=0)
-    return sinogram
-'''
+
 def radon_transform(image, theta):
     rows, cols = image.shape
     diagonal = int(np.sqrt(rows**2 + cols**2))
     pad_width = (diagonal - rows) // 2
-    padded_image = np.pad(image, pad_width, mode='constant', constant_values=0)
+    padded_image = np.pad(image, pad_width, mode='constant', constant_values=0) #padding
 
-    projections = []
+    projections = [] #save projection result
     for angle in theta:
-        rotated_image = rotate(padded_image, -angle, reshape=False, order=1)
-        projection = np.sum(rotated_image, axis=0)
-        projections.append(projection)
+        rotated_image = rotate(padded_image, -angle, reshape=False, order=1)# rotate image
+        projection = np.sum(rotated_image, axis=0) # get projection of one angle
+        projections.append(projection) 
     return np.array(projections)
 
-def inverse_radon_transform(sinogram, theta, size,filter):
+def inverse_radon_transform(sinogram, theta, size,filter_f):
     num_projections, num_detectors = sinogram.shape
     reconstruction = np.zeros((size, size))
-    center = size // 2
+    center = size // 2 # backprojection from the image center
     freqs = np.fft.fftfreq(num_detectors)
     ramp_filter = np.abs(freqs)
 
-    for i in range(len(theta)):#num_projections
+    for i in range(len(theta)):
         projection = sinogram[i, :]
-        projection_fft = fft(projection)
-        if(filter):
-            filtered_projection = np.real(ifft(projection_fft * ramp_filter))
+        projection_fft = fft(projection) #Fourier transform
+        if(filter_f):
+            filtered_projection = np.real(ifft(projection_fft * ramp_filter)) # multiply by filter、inverse Fourier transform
         else:
-            filtered_projection = np.real(ifft(projection_fft))  # 去掉滤波器
+            #filter in spatial domain
+            spatial_filter = np.real(np.fft.fftshift(ifft(ramp_filter)))#inverse Fourier transform、move DC to the center
+            filtered_projection = np.convolve(projection, spatial_filter, mode='same') #spatial domain do convolution
+
         angle = np.deg2rad(theta[i]+90)
         cos_a, sin_a = np.cos(angle), np.sin(angle)
+        #backprojection
         for x in range(size):
             for y in range(size):
-                t = (x - center) * cos_a + (y - center) * sin_a
-                t_idx = int(round(t + num_detectors // 2))
+                t = (x - center) * cos_a + (y - center) * sin_a #t can be + or -
+                #num_detectors // 2=>let the detector's center corresponds to the middle of the index
+                t_idx = int(round(t + num_detectors // 2)) 
                 if 0 <= t_idx < num_detectors:
                     reconstruction[x, y] += filtered_projection[t_idx]
-
-    reconstruction *= np.pi / (2 * len(theta))
     return reconstruction
 
 # 主程式
-img = rgb2gray(imread("C:\\Users\\User\\Desktop\\toy.jpg"))
+img = rgb2gray(imread("./image.jpg"))
 img_resized = resize(img, (512, 512))
-theta = np.arange(0., 180., .3)
-sinogram = radon_transform(img_resized, theta)
-nof_reconstruction = inverse_radon_transform(sinogram, theta, size=512,filter=False)
-reconstruction = inverse_radon_transform(sinogram, theta, size=512,filter=True)
+theta_03 = np.arange(0., 180., .3)
+sinogram_03 = radon_transform(img_resized, theta_03)
+f_reconstruction_03 = inverse_radon_transform(sinogram_03, theta_03, size=512,filter_f=True)
+s_reconstruction_03 = inverse_radon_transform(sinogram_03, theta_03, size=512,filter_f=False)
+
+theta_3 = np.arange(0., 180.,3)
+sinogram_3 = radon_transform(img_resized, theta_3)
+f_reconstruction_3 = inverse_radon_transform(sinogram_3, theta_3, size=512,filter_f=True)
+s_reconstruction_3 = inverse_radon_transform(sinogram_3, theta_3, size=512,filter_f=False)
 # 顯示結果
-fig, axs = plt.subplots(1, 4, figsize=(15, 5))
-axs[0].imshow(img_resized, cmap='gray')
-axs[0].set_title('Original Image')
-axs[1].imshow(sinogram.T, cmap='gray', aspect='auto')
-axs[1].set_title('Sinogram')
-axs[2].imshow(nof_reconstruction, cmap='gray')
-axs[2].set_title('Reconstructed Image(no filter)')
-axs[3].imshow(reconstruction, cmap='gray')
-axs[3].set_title('Reconstructed Image')
+fig, axs = plt.subplots(2, 4, figsize=(15, 5))
+axs[0,0].imshow(img_resized, cmap='gray')
+axs[0,0].set_title('Original Image')
+axs[0,1].imshow(sinogram_3.T, cmap='gray', aspect='auto')
+axs[0,1].set_title('Sinogram 3')
+axs[0,2].imshow(f_reconstruction_3, cmap='gray')
+axs[0,2].set_title('Reconstructed Image 3_frequency domain')
+axs[0,3].imshow(s_reconstruction_3, cmap='gray')
+axs[0,3].set_title('Reconstructed Image 3_spatial domain')
+
+axs[1,0].imshow(img_resized, cmap='gray')
+axs[1,0].set_title('Original Image')
+axs[1,1].imshow(sinogram_03.T, cmap='gray', aspect='auto')
+axs[1,1].set_title('Sinogram 03')
+axs[1,2].imshow(f_reconstruction_03, cmap='gray')
+axs[1,2].set_title('Reconstructed Image 03_frequency domain')
+axs[1,3].imshow(s_reconstruction_03, cmap='gray')
+axs[1,3].set_title('Reconstructed Image 03_spatial domain')
 plt.tight_layout()
 plt.show()
